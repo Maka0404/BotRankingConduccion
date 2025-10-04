7d5f46eeae5d912cb90ebfb1f4352d019ae8ccd4
nataliawg <natalia.watson@utec.edu.pe>
2025-10-03 23:15:44 -0500

    ya ta

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
-import { useState } from "react";
-import MainTabs from "./components/Tabs/MainTabs";
 import ConductoresPage from "./components/Conductores/ConductoresPage";
-import RankingPage from "./components/Ranking/RankingPage";
 import Navbar from "./components/NavBar";
 import Footer from "./components/Footer";
-import ChatbotButtonAndWindow from './components/ChatbotButtonAndWindow'; // Importa el nuevo componente
+import ChatbotButtonAndWindow from "./components/ChatbotButtonAndWindow";
 
-
-function App() {
-  const [activeTab, setActiveTab] = useState("conductores");
-
+export default function App() {
   return (
     <div className="flex flex-col min-h-screen">
-      {/* Menú superior */}
+    
       <Navbar />
 
-      {/* Contenido principal */}
       <main
         className="flex-grow"
         style={{
@@ -25,21 +18,12 @@ function App() {
           textAlign: "left",
         }}
       >
-        <MainTabs activeTab={activeTab} setActiveTab={setActiveTab} />
-        {activeTab === "conductores" ? (
         <ConductoresPage />
-        ) : (
-          <RankingPage />
-        )}
       </main>
 
-      {/* ¡Agrega el componente del chat aquí! */}
       <ChatbotButtonAndWindow />
 
-      {/* Footer */}
       <Footer />
     </div>
   );
 }
-
-export default App;
